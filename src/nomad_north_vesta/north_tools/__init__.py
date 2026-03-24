99c29089efe91299298216a9ec76ace56bf04225
mkuehbach <markus.kuehbach@physik.hu-berlin.de>
2026-03-24 12:37:46 +0100

    cruft update, manual resolving of hunks

---
+++ src/nomad_north_vesta/north_tools/__init__.py
@@ -0,0 +1,28 @@
+from nomad.config.models.north import NORTHTool
+from nomad.config.models.plugins import NorthToolEntryPoint
+
+vesta = NORTHTool(
+    short_description='Use VESTA (Visualization for Electronic STructural Analysis) in NOMAD.',
+    image='ghcr.io/fairmat-nfdi/nomad-north-vesta:main',
+    description="""### **VESTA**:
+
+    [VESTA is a software program used for visualizing and analyzing crystal structures.](https://jp-minerals.org/vesta/en/)
+
+    [Research article about the software](https://doi.org/10.1107/S0021889808012016)""",
+    external_mounts=[],
+    file_extensions=['vesta, cif, cry, exp, fcf, ics, ins, in, min, mol, pdb, struct, vasp, wrl, xyz'],
+    icon='https://raw.githubusercontent.com/FAIRmat-NFDI/nomad-north-vesta/main/src/nomad_north_vesta/north_tools/vesta/vesta.png',
+    image_pull_policy='Always',
+    default_url='/desktop',
+    maintainer=[{'email': 'markus.kuehbach@physik.hu-berlin.de', 'name': 'Markus Kühbach'}],
+    mount_path='/home/jovyan',
+    path_prefix='lab/tree',
+    privileged=False,
+    with_path=True,
+    display_name='vesta',
+)
+
+north_entry_point = NorthToolEntryPoint(
+    id_url_safe='nomad-north-vesta-vesta',
+    north_tool=vesta,
+)
--- src/nomad_north_vesta/north_tools/vesta/README.md
+++ src/nomad_north_vesta/north_tools/vesta/README.md
@@ -1,10 +1,26 @@
 # vesta - NORTH tool
 
 This directory contains the configuration for the vesta NORTH (NOMAD Remote Tools Hub) tool and NOMAD plugin.
-
 ## Quick start
 
 The vesta NORTH tool provides a containerized environment defined in a `NORTHTool` definition, a `NorthToolEntryPoint`, and a Dockerfile.
+
+## Building and testing
+
+Build the Docker image locally:
+
+```bash
+docker build -f src/nomad_north_vesta/north_tools/vesta/Dockerfile \
+    -t ghcr.io/fairmat-nfdi/nomad-north-vesta:latest .
+```
+
+Test the image (for jupyter notebook image):
+
+```bash
+docker run -p 8888:8888 ghcr.io/fairmat-nfdi/nomad-north-vesta:latest
+```
+
+Access JupyterLab at `http://localhost:8888`.
 
 ## Documentation
 
